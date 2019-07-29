570f22ade7a3bdbc5a6849e3acd7ea43d5cb4b1f
ryuyaminamida <ryuya.minamida@hokusys.jp>
2019-07-29 18:02:35 +0900

    コメントの挿入とサーブレットの統合

--- src/worldcup/SearchServlet.java
+++ src/worldcup/SearchServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/SearchServlet")
 public class SearchServlet extends HttpServlet {
@@ -28,37 +29,71 @@ public class SearchServlet extends HttpServlet {
 
 		RequestDispatcher rd;
 
+
+
+
 		if("選手検索".equals(btn)) {
 
+			// 入力された選手IDの取得とInt変換
 			String id  = request.getParameter("player_id");
 			int plid = Integer.valueOf(id);
 
+			// 入力されたIDをもとに選手情報をBeanクラスにセット
 			Player player = new Player();
-
 			PlayerBean bean = player.getPlayer(plid);
 
+			// Beanクラスに選手情報がセットされたか確認。(されていない場合、検索ページに留まる)
 			if(bean != null) {
-				rd = request.getRequestDispatcher("./PlayerServlet");
+//				Player player = new Player();
+
+				HttpSession session = request.getSession();
+//				String id  = request.getParameter("player_id");
+//				int plid = Integer.valueOf(id);
+
+				PlayerBean pl = player.getPlayer(plid);
+
+				session.setAttribute("user_db", pl);
+
+				rd = request.getRequestDispatcher("./WorldCup/player_result.jsp");
+				rd.forward(request, response);
+
+
+
+//				rd = request.getRequestDispatcher("./PlayerServlet");
 			} else {
 				rd = request.getRequestDispatcher("./WorldCup/searchid.jsp");
 			}
-			rd.forward(request, response);
+
 
 		} else if("国指定検索".equals(btn)) {
 
+			// 入力された国IDの取得とInt変換
 			String id  = request.getParameter("country_id");
 			int coid = Integer.valueOf(id);
 
+			// 入力されたIDをもとに選手情報をBeanリストにセット
 			Country country = new Country();
-
 			ArrayList<PlayerBean> beanList = country.getCountry(coid);
 
+			// Beanリストに選手情報がセットされたか確認。(されていない場合、検索ページに留まる)
 			if(beanList != null) {
-				rd = request.getRequestDispatcher("./CountryServlet");
+//				String id  = request.getParameter("country_id");
+//				int coid = Integer.valueOf(id);
+
+//				Country country = new Country();
+//				ArrayList<PlayerBean> playerList = country.getCountry(coid);
+
+				request.setAttribute("playerList", beanList);
+
+
+				rd = request.getRequestDispatcher("./WorldCup/country_result.jsp");
+				rd.forward(request, response);
+
+
+//				rd = request.getRequestDispatcher("./CountryServlet");
 			} else {
 				rd = request.getRequestDispatcher("./WorldCup/searchid.jsp");
 			}
-			rd.forward(request, response);
 
 		}
 	}
@@ -68,8 +103,7 @@ public class SearchServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		// 今回はdoPostで処理
+		// Getで呼び出されてもPostで再呼び出し
 		doPost(request, response);
 	}
 }
